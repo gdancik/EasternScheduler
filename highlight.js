56b86020e54baad2d85c69c6604ee08ff2ca52b0
CalebJones94 <31103185+CalebJones94@users.noreply.github.com>
2017-09-01 16:53:22 -0400

    Update highlight.js

--- highlight.js
+++ highlight.js
@@ -1,6 +1,8 @@
 // This script highlights all occurences of 'qry'. The variable 'qry' should
 // be set before calling this script
 
+// alert("in highlight.js");
+
 Course = class {
     constructor(Subj, Crse, days, times) {
         this.Subj = Subj;
@@ -8,14 +10,13 @@ Course = class {
         this.days = days;
         this.times = times;
     }
-    print(var i) {
-
-        // get unique starting and ending times
+    print() {
+		 // get unique starting and ending times
         var starts = Array.from(new Set(this.times['start']));
         var ends = Array.from(new Set(this.times['end']));
         if (starts.length == 1 && ends.length == 1) {
             console.log(this.Subj + "-" + this.Crse + ", " + this.days.join("") + " " + 
-                    classTimes[i][0] + "-" + classTimes[i][1]);
+                    starts[0] + "-" + ends[0]);
         } else {
             console.log(this.Subj + "-" + this.Crse + ", days/times vary");
         }
@@ -23,13 +24,15 @@ Course = class {
     }
 };
 
+
 if (typeof(qry) == "undefined") {
     qry = "Connecticut";
 } else if (qry == "" || qry == " ") {
     qry = "Connecticut";
 }
 
-alert(" highlight.js with qry = " + qry);
+//alert(" highlight.js with qry = " + qry);
+
 console.log("highlighter is running with qry = " + qry + "...");
 
 // get all HTML from the body
@@ -41,6 +44,7 @@ var html = document.getElementsByTagName('table');
 
 var commaCheck = ((qry.length - qry.replace(/,/g, "").length)+1);
 
+//alert("You've got " + commaCheck + " courses"); 
 /*
 checks the amount of commas and adds one to represent the amount of courses if you would like
 a variable amount of course inputs*/
@@ -50,7 +54,7 @@ var courseSet = [];
 var inputCourse = qry.split(", ");
 
 for (var i=0; i < commaCheck; i++){
-	var courseSet[i] = inputCourse[i];
+	courseSet[i] = inputCourse[i];
 }
 
 /*
@@ -60,12 +64,14 @@ var x = inputCourse[0];
 var y = inputCourse[1];
 var z = inputCourse[2];*/
 
-var classCourse = [][2];
+var classCourse = [];
 
-var classTimes[][2];
+var classTimes = [commaCheck][2];
 
-for (var i = 1; i <commaCheck; i++){
-	var classSet = courseSet[i].split(" ");
+for (var i = 0; i <commaCheck; i++){
+	classSet = courseSet[i].split(" ");
+	console.log("got: " + classSet[0] + ", " + classSet[1]);
+	classCourse[i] = new Array(2);
 	classCourse[i][0] = classSet[0];
 	classCourse[i][1] = classSet[1];
 }
@@ -83,9 +89,6 @@ var zClassCourse = z.split(" ");
 var zSubj = zClassCourse[0];
 var zCrse = zClassCourse[1];*/
 
-console.log("x = " + x);
-console.log("y = " + y);
-
 var COURSES = [];
 var COURSE_NUM = 0;
 
@@ -96,9 +99,10 @@ for (tableIndex = 3; tableIndex < html.length-1; tableIndex++) {
 
   for (var i = 3; i < rows.length; i++) {
 	  
-	for(var i = 0; i < commaCheck; i++){
+	for(var j = 0; j < commaCheck; j++){
 	    
-		if (rows[i].cells[3].innerText == classCourse[i][0] && rows[i].cells[4].innerText == classCourse[i][1]) {
+		
+		if (rows[i].cells[3].innerText == classCourse[j][0] && rows[i].cells[4].innerText == classCourse[j][1]) {
 		var subj = rows[i].cells[3].innerText;
 		var crse = rows[i].cells[4].innerText;
 		var dys = rows[i].cells[10].innerText;
@@ -107,12 +111,11 @@ for (tableIndex = 3; tableIndex < html.length-1; tableIndex++) {
 
 		//console.log("Course: " + subj + "-" + crse + "  Days: " + dys + "  Time: " + tme);
 		
+
 		var timeTable = tme.split("-");
-			classTimes[i][0] = timeTable[0];
-			classTimes[i][1] = timeTable[1];
 			
 		var course1 = new Course(subj, crse, dys.split(""), 
-		       {"start": classTimes[i][0], "end": classTimes[i][1]} );
+		       {"start": timeTable[0], "end": timeTable[1]} );
 		
 		COURSES[COURSE_NUM] = course1;
 		COURSE_NUM++;
@@ -131,37 +134,21 @@ for (var i = 0; i < COURSE_NUM; i++) {
 	COURSES[i].print(i);
 }
 
-/*
-for (i = 0; i < rows.length; i++) {
-	if (rows[i].cells.length > 5) {
-		var subj = rows[i].cells[3].innerText;
-		var crse = rows[i].cells[4].innerText;
-		// do something similar to pull out the days/times
-		console.log("Course: " + subj + "-" + crse);
-		
-	}
+
+var newWindow = window.open("", null);
+newWindow.document.write("<h1> This is a new page </h1>");
+newWindow.document.write("<p style=\"color:red\"> This is a new page </p>");
+newWindow.document.write("<p> This is a new page </p>");
+
+function writeParagraph(w, txt) {
+	var s = "<p>" + txt + "</p>";
+	w.document.write(s);
 }
 
-*/
-/*for (i = 0; i < html.length; i++) {
-	if (qry == html[i].cells[3].innerHTML) {
-		//var x = html[i].cells[3].innerHTML;
-		//var y = html[i].cells[10].innerHTML;
-		console.log("yo");
-	}
-}*/
-
-//  change HTML so word is highlighted
-//var findWord = RegExp(qry, "g"); // will replace all occurences 
-
-//var replaceWord = "<span style='color:red'>" + qry + "</span>"
-//text = text.replace(findWord, replaceWord);
-
-// reset the inner HTML of the body
-//document.getElementsByTagName('table')[0].innerHTML = text;
+writeParagraph(newWindow, "This is a test");
+writeParagraph(newWindow, "This is not a test");
 
 
 
-//1. Have the user enter a course number CSC 210 for example
-//2. When Highlihgt is clicked it will output the course information
-//3. 
+
+
